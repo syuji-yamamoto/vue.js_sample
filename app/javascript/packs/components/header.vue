0a3d5ac25af9343e3a45ec3b4f64cab8fc755116
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-02-01 11:45:09 +0900

    コンポーネントの表示

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ul id="dropdown" class="dropdown-content">
-      <li><a href="#">Top</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><router-link to="/">Top</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
     </ul>
     <nav>
       <div class="nav-wrapper container">
---
+++ app/javascript/packs/components/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>一覧表示画面</p>
+    <p>レッドエンジェル</p>
+  </div>
+</template>
